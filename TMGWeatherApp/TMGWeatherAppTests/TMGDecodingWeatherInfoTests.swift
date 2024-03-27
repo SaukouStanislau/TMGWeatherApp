101731da0a52c231139502b45ad061525d01d118
Stas <stanislav.savkov@oxagile.com>
2024-03-27 19:04:31 +0100

    Add tests to cover decoding WeatherInfo

--- TMGWeatherApp/TMGWeatherAppTests/TMGDecodingWeatherInfoTests.swift
+++ TMGWeatherApp/TMGWeatherAppTests/TMGDecodingWeatherInfoTests.swift
@@ -9,4 +9,35 @@ import XCTest
 @testable import TMGWeatherApp
 
 final class TMGDecodingWeatherInfoTests: XCTestCase {
+    func testWeatherStatusDecodingSuccess()  throws {
+        let jsonWithWeatherStatus = """
+            {
+                "weather": [
+                    {
+                        "description": "clear sky",
+                        "main": "Clear"
+                    }
+                ]
+            }
+        """.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        let weatherInfo = try decoder.decode(WeatherInfo.self, from: jsonWithWeatherStatus)
+
+        XCTAssertEqual(weatherInfo.weather?.first?.description, "clear sky")
+        XCTAssertEqual(weatherInfo.weather?.first?.status, "Clear")
+    }
+
+    func testTemperatureDecodingSuccess()  throws {
+        let jsonWithTemperature = """
+            {
+                "main": {
+                    "temp": 286.77
+                }
+            }
+        """.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        let weatherInfo = try decoder.decode(WeatherInfo.self, from: jsonWithTemperature)
+
+        XCTAssertEqual(weatherInfo.temperatureInfo?.temperature, 286.77)
+    }
 }
