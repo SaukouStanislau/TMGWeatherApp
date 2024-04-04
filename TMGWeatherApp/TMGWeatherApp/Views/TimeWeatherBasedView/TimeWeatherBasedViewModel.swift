0fe722ea348d8292a6e9d97d3cf7db21f0108dc6
Stas <stanislav.savkov@oxagile.com>
2024-04-04 17:17:45 +0200

    constats created for TimeWeatherBasedView

--- TMGWeatherApp/TMGWeatherApp/Views/TimeWeatherBasedView/TimeWeatherBasedViewModel.swift
+++ TMGWeatherApp/TMGWeatherApp/Views/TimeWeatherBasedView/TimeWeatherBasedViewModel.swift
@@ -10,39 +10,8 @@ import SwiftUI
 struct TimeWeatherBasedViewModel {
     let weatherInfo: WeatherInfo?
 
-    let hoursMultipliersDictionary = [
-        (0..<5): 0.1,
-        (5..<6): 0.2,
-        (6..<7): 0.5,
-        (8..<10): 0.7,
-        (10..<12): 0.8,
-        (12..<17): 1.0,
-        (17..<18): 0.8,
-        (18..<19): 0.7,
-        (19..<21): 0.4,
-        (21..<22): 0.2,
-        (23..<24): 0.1,
-    ]
-
-    let defaultTimeMultiplier = 1.0
-    let defaultWeatherMultiplier = 1.0
-    let maximumBrightness: CGFloat = 1.0
-    let minimumBrightness: CGFloat = 0.1
-
-    let weatherMultipliersDictionary = [
-        WeatherInfo.WeatherStatus.Status.clear : 1.0,
-        WeatherInfo.WeatherStatus.Status.snow: 0.8,
-        WeatherInfo.WeatherStatus.Status.clouds: 0.8,
-        WeatherInfo.WeatherStatus.Status.drizzle: 0.7,
-        WeatherInfo.WeatherStatus.Status.rain: 0.6,
-        WeatherInfo.WeatherStatus.Status.mist: 0.5,
-        WeatherInfo.WeatherStatus.Status.smoke: 0.5,
-        WeatherInfo.WeatherStatus.Status.thunderstorm: 0.4
-    ]
-
     var currentHour: Int {
-        debugPrint(currentHourInLocation ?? Calendar.current.component(.hour, from: Date()))
-        return currentHourInLocation ?? Calendar.current.component(.hour, from: Date())
+        currentHourInLocation ?? Calendar.current.component(.hour, from: Date())
     }
 
     var currentHourInLocation: Int? {
@@ -54,11 +23,10 @@ struct TimeWeatherBasedViewModel {
     }
 
     var brightnessMultiplier: CGFloat {
-        let interval = hoursMultipliersDictionary.keys.first { $0.contains(currentHour) } ?? (0..<0)
-        let timeMultiplier = hoursMultipliersDictionary[interval] ?? defaultTimeMultiplier
-        let weatherMultiplier = weatherMultipliersDictionary[weatherInfo?.weather?.first?.status ?? .clear] ?? defaultWeatherMultiplier
+        let interval = Constants.hoursMultipliersDictionary.keys.first { $0.contains(currentHour) } ?? (0..<0)
+        let timeMultiplier = Constants.hoursMultipliersDictionary[interval] ?? Constants.defaultTimeMultiplier
+        let weatherMultiplier = Constants.weatherMultipliersDictionary[weatherInfo?.weather?.first?.status ?? .clear] ?? Constants.defaultWeatherMultiplier
 
-        debugPrint(timeMultiplier)
         return timeMultiplier * 0.9 * weatherMultiplier
     }
 
@@ -68,5 +36,39 @@ struct TimeWeatherBasedViewModel {
 
     var bottomColor: Color {
         Color(red: 0.218 * brightnessMultiplier, green: 0.833 * brightnessMultiplier, blue: 1.0 * brightnessMultiplier)
+    }
+}
+
+private extension TimeWeatherBasedViewModel {
+    struct Constants {
+        static let defaultTimeMultiplier = 1.0
+        static let defaultWeatherMultiplier = 1.0
+        static let maximumBrightness: CGFloat = 1.0
+        static let minimumBrightness: CGFloat = 0.1
+
+        static let hoursMultipliersDictionary = [
+            (0..<5): 0.1,
+            (5..<6): 0.2,
+            (6..<7): 0.5,
+            (8..<10): 0.7,
+            (10..<12): 0.8,
+            (12..<17): 1.0,
+            (17..<18): 0.8,
+            (18..<19): 0.7,
+            (19..<21): 0.4,
+            (21..<22): 0.2,
+            (23..<24): 0.1,
+        ]
+
+        static let weatherMultipliersDictionary = [
+            WeatherInfo.WeatherStatus.Status.clear : 1.0,
+            WeatherInfo.WeatherStatus.Status.snow: 0.8,
+            WeatherInfo.WeatherStatus.Status.clouds: 0.8,
+            WeatherInfo.WeatherStatus.Status.drizzle: 0.7,
+            WeatherInfo.WeatherStatus.Status.rain: 0.6,
+            WeatherInfo.WeatherStatus.Status.mist: 0.5,
+            WeatherInfo.WeatherStatus.Status.smoke: 0.5,
+            WeatherInfo.WeatherStatus.Status.thunderstorm: 0.4
+        ]
     }
 }
